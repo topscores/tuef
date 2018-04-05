1c658516af4cfc3f3b6db842782ab3b1c1095a09
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2018-04-05 21:46:19 +0700

    Add utils and unit tests

---
+++ libs/__tests__/utils.js
@@ -0,0 +1,89 @@
+import {
+  isValidDate,
+  isValueAvailable,
+  lpad,
+  rpad,
+  truncate,
+  yyyymmdd,
+} from '../utils'
+
+describe('utils', () => {
+  describe('isValidate', () => {
+    it('return true for valid date', () => {
+      const date = new Date()
+      expect(isValidDate(date)).toEqual(true)
+    })
+    it('return false for not valid date', () => {
+      const date = new Date('')
+      expect(isValidDate(date)).toEqual(false)
+    })
+  })
+  describe('isValueAvailable', () => {
+    it('return false for undefined', () => {
+      expect(isValueAvailable(undefined)).toEqual(false)
+    })
+    it('return false for null', () => {
+      expect(isValueAvailable(null)).toEqual(false)
+    })
+  })
+  describe('lpad', () => {
+    it('convert input to string before padding', () => {
+      expect(lpad(12, 5)).toEqual('   12')
+      expect(lpad(12, 5, 0)).toEqual('00012')
+      expect(lpad(12, 5, 'x')).toEqual('xxx12')
+    })
+    it('left pad string shorter than specified length', () => {
+      expect(lpad('12', 5)).toEqual('   12')
+      expect(lpad('12', 5, 0)).toEqual('00012')
+      expect(lpad('12', 5, 'x')).toEqual('xxx12')
+    })
+    it('does not modify string longer than or equal to specified length', () => {
+      expect(lpad('12113', 5)).toEqual('12113')
+      expect(lpad('1211345', 5, 0)).toEqual('1211345')
+      expect(lpad('1211345', 5, 'x')).toEqual('1211345')
+    })
+  })
+  describe('rpad', () => {
+    it('convert input to string before padding', () => {
+      expect(rpad(12, 5)).toEqual('12   ')
+      expect(rpad(12, 5, 0)).toEqual('12000')
+      expect(rpad(12, 5, 'x')).toEqual('12xxx')
+    })
+    it('right pad string shorter than specified length', () => {
+      expect(rpad('12', 5)).toEqual('12   ')
+      expect(rpad('12', 5, 0)).toEqual('12000')
+      expect(rpad('12', 5, 'x')).toEqual('12xxx')
+    })
+    it('does not modify string longer than or equal to specified length', () => {
+      expect(rpad('12113', 5)).toEqual('12113')
+      expect(rpad('1211345', 5, 0)).toEqual('1211345')
+      expect(rpad('1211345', 5, 'x')).toEqual('1211345')
+    })
+  })
+  describe('truncate', () => {
+    it('convert input to string before truncating', () => {
+      expect(truncate(11111, 3)).toEqual('111')
+      expect(truncate(11, 3)).toEqual('11')
+    })
+    it('truncate string longer than specified length', () => {
+      expect(truncate('11111', 3)).toEqual('111')
+    })
+    it('does not modify string shorter than specified length', () => {
+      expect(truncate('11', 3)).toEqual('11')
+    })
+  })
+  describe('yyyymmdd', () => {
+    it('convert date to YYYYMMDD', () => {
+      let dateStr
+      dateStr = yyyymmdd(new Date('2016-8-1'))
+      expect(dateStr).toEqual('20160801')
+      dateStr = yyyymmdd(new Date('2016-10-12'))
+      expect(dateStr).toEqual('20161012')
+    })
+    it('throw error if argument is not Date type', () => {
+      const dateStr = '2016-8-1'
+      expect(yyyymmdd.bind(null, dateStr)).toThrow(TypeError)
+      expect(yyyymmdd.bind(null, new Date('abc'))).toThrow(TypeError)
+    })
+  })
+})
---
+++ libs/utils.js
@@ -0,0 +1,47 @@
+export const lpad = (val, length, padStr = ' ') => {
+  let result = val.toString()
+  while (result.length < length) {
+    result = `${padStr}${result}`
+  }
+  return result
+}
+
+export const rpad = (val, length, padStr = ' ') => {
+  let result = val.toString()
+  while (result.length < length) {
+    result = `${result}${padStr}`
+  }
+  return result
+}
+
+export const truncate = (val, length) => {
+  let result = val.toString()
+  if (result.length > length) {
+    result = result.substr(0, length)
+  }
+  return result
+}
+
+export const isValidDate = date => {
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    return false
+  }
+  return !isNaN(date.getTime())
+}
+
+export const yyyymmdd = date => {
+  // If date is a Date object
+  if (isValidDate(date)) {
+    const yyyy = date.getFullYear().toString()
+    const mm = (date.getMonth() + 1).toString() // getMonth() is zero-based
+    const dd = date.getDate().toString()
+    return yyyy + (mm[1] ? mm : `0${mm[0]}`) + (dd[1] ? dd : `0${dd[0]}`) // zero pad
+  }
+  // If date is not a Date object
+  const type = Object.prototype.toString.call(date)
+  throw new TypeError(`yyyymmdd expects input of type Date, but get ${type}`)
+}
+
+export const isValueAvailable = val => {
+  return val !== undefined && val !== null
+}
