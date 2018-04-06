6ddacc58a0794aadea042b2cc24cd44fa29d98d5
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2018-04-06 11:45:54 +0700

    Add TUEF Field and tests

---
+++ libs/Field.js
@@ -0,0 +1,75 @@
+import util from 'util'
+import { isType } from './isType'
+import { isValueAvailable, lpad, rpad, truncate, yyyymmdd } from './utils'
+
+export default class Field {
+  constructor(spec, val, defaultVal) {
+    this.spec = spec
+    this.val = val || defaultVal
+  }
+
+  toString(lengthType = 'vary') {
+    let result
+    const spec = this.spec
+    // Check for blank field
+    if (!isValueAvailable(this.val) || this.val === '') {
+      // Required field cannot be blank
+      if (spec.required) {
+        throw new TypeError(
+          `Required field ${spec.name} not found in ${util.inspect(this)}`
+        )
+      }
+      // This field is optional return empty string
+      return ''
+    }
+
+    // Validate input type
+    if (!isType(spec.type, this.val)) {
+      throw new TypeError(
+        `${spec.name} must be ${spec.type}` +
+          `, Field data = ${util.inspect(this.data)}`
+      )
+    }
+
+    // this.length is a required field
+    if (spec.length !== undefined) {
+      // Zero padding for type N
+      if (spec.type === 'N') {
+        result = lpad(this.val, spec.length, 0)
+
+        // For Type A and AN convert to upper case before padding
+      } else if (spec.type === 'A' || spec.type === 'AN') {
+        result = rpad(this.val.toUpperCase(), spec.length, ' ')
+
+        // Type P can be any printable letter include upper and lower case
+      } else {
+        result = rpad(this.val, spec.length)
+      }
+      result = truncate(result, spec.length)
+
+      // Add tag and length(2 digits) as a prefix
+      if (lengthType === 'vary') {
+        // Variable length field need tag
+        if (spec.tag !== undefined) {
+          // convert length 1 - 9 to 01 - 09
+          const resultLength = lpad(result.length, 2, 0)
+          result = spec.tag + resultLength + result
+        } else {
+          throw new Error(
+            `Missing tag property in ${spec.name} field` +
+              `, Field data = ${util.inspect(this)}`
+          )
+        }
+      }
+
+      // Throw error if this.length is not provided
+    } else {
+      throw new Error(
+        `Missing length property in ${spec.name} field` +
+          `Field data = ${util.inspect(this)}`
+      )
+    }
+
+    return result
+  }
+}
---
+++ libs/__tests__/Field.js
@@ -0,0 +1,133 @@
+import Field from '../Field'
+
+describe('TUEF Field', () => {
+  describe('toString', () => {
+    describe('Validate input before converting', () => {
+      it('Throws error if val for a required field is not available', () => {
+        const spec = {
+          name: 'test',
+          tag: 't1',
+          type: 'A',
+          required: true,
+          length: 10,
+        }
+
+        const field = new Field(spec, '')
+        expect(() => {
+          field.toString()
+        }).toThrow(TypeError)
+      })
+      it('Throws error if val is not of a valid type', () => {
+        const spec = {
+          name: 'test',
+          tag: 't1',
+          type: 'A',
+          required: true,
+          length: 10,
+        }
+
+        const field = new Field(spec, '12345')
+        expect(() => {
+          field.toString()
+        }).toThrow(TypeError)
+      })
+      it('Throws error if tag is not available for variable type field', () => {
+        const spec = {
+          name: 'test',
+          type: 'A',
+          required: true,
+          length: 10,
+        }
+
+        const field = new Field(spec, 'hello')
+        expect(() => {
+          field.toString()
+        }).toThrow(Error)
+      })
+      it('Throws error if length is not available', () => {
+        const spec = {
+          name: 'test',
+          tag: 't1',
+          type: 'A',
+          required: true,
+        }
+
+        const field = new Field(spec, 'hello')
+        expect(() => {
+          field.toString()
+        }).toThrow(Error)
+      })
+    })
+    describe('Converts variable length field', () => {
+      it('Uses defaultValue instead of val if not available', () => {
+        const spec = {
+          name: 'test',
+          tag: 't1',
+          type: 'A',
+          required: true,
+          length: 10,
+        }
+
+        const field = new Field(spec, '', 'default')
+        expect(field.toString()).toEqual('t110DEFAULT   ')
+      })
+      it('Converts val to toef string', () => {
+        const spec = {
+          name: 'test',
+          tag: 't1',
+          type: 'A',
+          required: true,
+          length: 10,
+        }
+
+        const field = new Field(spec, 'heLLo')
+        expect(field.toString()).toEqual('t110HELLO     ')
+      })
+      it('Returns empty string if field is optional and val is empty', () => {
+        const spec = {
+          name: 'test',
+          tag: 't1',
+          type: 'A',
+          length: 10,
+        }
+
+        const field = new Field(spec, '')
+        expect(field.toString()).toEqual('')
+      })
+    })
+    describe('Convert fixed length field', () => {
+      it('Uses defaultValue instead of val if not available', () => {
+        const spec = {
+          name: 'test',
+          type: 'A',
+          required: true,
+          length: 10,
+        }
+
+        const field = new Field(spec, '', 'default')
+        expect(field.toString('fixed')).toEqual('DEFAULT   ')
+      })
+      it('Converts val to toef string', () => {
+        const spec = {
+          name: 'test',
+          type: 'A',
+          required: true,
+          length: 10,
+        }
+
+        const field = new Field(spec, 'heLLo')
+        expect(field.toString('fixed')).toEqual('HELLO     ')
+      })
+      it('Returns empty string if field is optional and val is empty', () => {
+        const spec = {
+          name: 'test',
+          type: 'A',
+          length: 10,
+        }
+
+        const field = new Field(spec, '')
+        expect(field.toString('fixed')).toEqual('')
+      })
+    })
+  })
+})
