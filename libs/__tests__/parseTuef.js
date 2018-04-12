a9455e7aa7ba152d0e9340640e88ca0e83f4ec18
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2018-04-12 13:34:58 +0700

    Add parseVarySegment and tests

--- libs/__tests__/parseTuef.js
+++ libs/__tests__/parseTuef.js
@@ -1,4 +1,8 @@
-import { parseFixedLengthSegment, parseTuef } from '../parseTuef'
+import {
+  parseFixedLengthSegment,
+  parseVaryLengthSegment,
+  parseTuef,
+} from '../parseTuef'
 
 describe('parseTuef', () => {
   describe('parseFixedLengthSegment', () => {
@@ -41,6 +45,86 @@ describe('parseTuef', () => {
         ],
       }
       const str = '00AHell      '
+      expect(() => {
+        parseFixedLengthSegment(spec, str)
+      }).toThrowError()
+    })
+  })
+  describe('parseVaryLengthSegment', () => {
+    it('Parses vary length segment', () => {
+      const spec = {
+        lengthType: 'vary',
+        fieldSpecs: [
+          {
+            tag: 'PN',
+            name: 'fieldA',
+            length: 30,
+            type: 'N',
+          },
+          {
+            tag: '01',
+            name: 'fieldB',
+            length: 10,
+            type: 'A',
+          },
+        ],
+      }
+      const str = 'PN02030110Hell      '
+      const { obj, parsedChar } = parseVaryLengthSegment(spec, str)
+
+      expect(obj).toEqual({ fieldA: 3, fieldB: 'Hell' })
+      expect(parsedChar).toEqual(20)
+    })
+
+    it('Parses vary length segment with optional fields', () => {
+      const spec = {
+        lengthType: 'vary',
+        fieldSpecs: [
+          {
+            tag: 'PN',
+            name: 'fieldA',
+            length: 30,
+            type: 'N',
+          },
+          {
+            tag: 'MS',
+            name: 'optionalField',
+            length: 5,
+            type: 'AN',
+          },
+          {
+            tag: '01',
+            name: 'fieldB',
+            length: 10,
+            type: 'A',
+          },
+        ],
+      }
+      const str = 'PN02030110Hell      '
+      const { obj, parsedChar } = parseVaryLengthSegment(spec, str)
+
+      expect(obj).toEqual({ fieldA: 3, fieldB: 'Hell' })
+      expect(parsedChar).toEqual(20)
+    })
+    it('Throw error if field value does not have correct type', () => {
+      const spec = {
+        lengthType: 'vary',
+        fieldSpecs: [
+          {
+            tag: 'PN',
+            name: 'fieldA',
+            length: 3,
+            type: 'N',
+          },
+          {
+            tag: '01',
+            name: 'fieldB',
+            length: 10,
+            type: 'A',
+          },
+        ],
+      }
+      const str = 'PN02A30110Hell      '
       expect(() => {
         parseFixedLengthSegment(spec, str)
       }).toThrowError()
--- libs/parseTuef.js
+++ libs/parseTuef.js
@@ -1,4 +1,5 @@
 import { isType } from './isType'
+
 export const parseFixedLengthSegment = (spec, str) => {
   const obj = {}
   let parsedChar = 0
@@ -19,7 +20,37 @@ export const parseFixedLengthSegment = (spec, str) => {
   return { obj, parsedChar }
 }
 export const parseVaryLengthSegment = (spec, str) => {
-  return str
+  const obj = {}
+  let parsedChar = 0
+
+  spec.fieldSpecs.forEach(fieldSpec => {
+    // Tag must be 2 characters
+    const tag = str.substr(parsedChar, 2)
+    // This is not this tag fieldSpec dont process
+    if (fieldSpec.tag !== tag) {
+      return
+    }
+    parsedChar += 2
+
+    // Length must be 2 character
+    const length = parseInt(str.substr(parsedChar, 2), 10)
+    parsedChar += 2
+
+    // Extract field value
+    const fieldVal = str.substr(parsedChar, length)
+    parsedChar += length
+
+    if (isType(fieldSpec.type, fieldVal)) {
+      obj[fieldSpec.name] =
+        fieldSpec.type === 'N' ? parseInt(fieldVal, 10) : fieldVal.trim()
+    } else {
+      throw Error(
+        `Expect ${fieldSpec.name} to be ${fieldSpec.type} but got ${fieldVal}`
+      )
+    }
+  })
+
+  return { obj, parsedChar }
 }
 export const parseTuefSegment = (spec, str) => {
   return spec.lengthType === 'fixed'
