cbf200ed4fc61bd0ca89ac050609a6542d697829
Arnupharp Viratanapanu <arnupharp.v@ittp.co.th>
2020-03-13 00:00:38 +0700

    Add .d.ts to support typescript

---
+++ tuef.d.ts
@@ -0,0 +1,44 @@
+declare module "tuef" {
+  export type FieldType = "A" | "N" | "AN" | "P";
+  export interface FixedLengthFieldSpec {
+    name: string;
+    type: FieldType;
+    length: number;
+    val?: number;
+    mapKey?: string;
+    mapFunc?: function;
+    required?: boolean;
+    defaultVal?: any;
+  }
+  export interface VaryLengthFieldSpec {
+    name: string;
+    type: FieldType;
+    tag: string;
+    length?: number;
+    val?: number;
+    mapKey?: string;
+    mapFunc?: function;
+    required?: boolean;
+    defaultVal?: any;
+  }
+  export type FieldSpec = VaryLengthFieldSpec | FixedLengthFieldSpec;
+  export type LengthType = "vary" | "fixed";
+  export interface SegmentSpec {
+    lengthType: LengthType;
+    fieldSpecs: Array<FieldSpec>;
+  }
+  export interface TuefSpec {
+    [key: string]: SegmentSpec;
+  }
+  export class Field {
+    constructor(spec: FieldSpec, val: any, defaultVal: any);
+    toString(lengthType?: LengthType): string;
+  }
+  export class Segment {
+    constructor(spec: SegmentSpec, data: string);
+    getSemicolonSeparatedHeader(): string;
+    toSemicolonSeparatedString(): string;
+    toString(): string;
+  }
+  export function parseTuef(spec: TuefSpec, str: string): object;
+}
